f4c1db68c13a3193e49c1f27bfc70b02c5e0f992
350887156 <350887156@qq.com>
2021-05-14 15:44:37 +0800

    bug fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-12 17:29:03
- * @LastEditTime: 2021-05-14 15:37:02
+ * @LastEditTime: 2021-05-14 15:43:30
  * @LastEditors: Sclea
  * @Description: In User Settings Edit
  * @FilePath: /example/lib/main.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:ali_auth/ali_auth.dart';
 import 'package:flutter/services.dart';
+
+import 'config.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,13 +33,8 @@ class _MyAppState extends State<MyApp> {
         body: ListView(
           children: [
             _action('init', () async {
-              //ios
-              final appKey =
-                  'FqkuICQmq6p56nf0eEW56DH87rDJkhpIhnmUm8B4VNsa2GpfX6usLW/12ANy3HJnLcYjQb88epymuft++tPIJ8th8943KcUYwD8QhKLPuYyWpi2/GxGgW0+n30NP6fKyuL6UYlG24F7/UaC0oXum1kG4fwdFgDmTI4jqlQC2kpbpdZUsCeNs1fGXmjFUUPwjKI6GMzzM+DaNwxlj+C1C8QDMgoV1/dEq6AqL21ijxm3We5f3hEzBcuDzRf9L0T9VpEgE4l7pIj8=';
-              final androidKey =
-                  'YlLUxHLm2Uw6fqTrLrPku8XNnTDEOIov7hZH2rpAXwFaenzsFGjbKziuCD8TCdjUNmwN7cb37DxvpHKJXt/mZnpjkyQVcR1fJqFYzbliJgSbp3C7CJoG6dzEYYH6iQ1v2/0Sbi9nD3H1t8arCO9RrgD4OXJwVvH1eVzQ8eQa3jOQSeFLXR6oW/8Eljp4HV4KmtIxaczs852mW07Cf0w/+06w1GfHmlocS05/XF59WBG31dIl+isNNmqprDBW7lWon/ItTCkkKus2/8u2Mxby0mWQSXc4bHBQHt0a7op/hYVgc3JKoxjskA==';
-              final result =
-                  await AliAuth.init(iOSKey: appKey, androidKey: androidKey);
+              final result = await AliAuth.init(
+                  iOSKey: Config.iOSKey, androidKey: Config.androidKey);
               print('flutter打印日志$result');
             }),
             _action('取消登录', () async {
